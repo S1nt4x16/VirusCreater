902c484a908399a7d57c566e334818dd45cd8a82
S1nt4x16 <50811498+S1nt4x16@users.noreply.github.com>
2019-08-02 16:03:52 +0700

    Add files via upload

---
+++ virusv16.sh
@@ -0,0 +1,48 @@
+clear
+toilet -f mono12 -F gay "WELCOME"
+echo -n 'Masukan Password Anda : ' | lolcat
+read pw;
+if [ $pw = 'SYNT4X16' ];
+then
+echo 'Password Anda Benar'
+else
+echo 'Password Anda Salah'
+fi
+
+clear
+echo $red
+cat img.txt
+echo "===================================>
+# Coded By : SYNT4X16
+
+# WEB : tutorialtermuxsynt4x16.wordpress.com
+
+# DONT FORGET VISIT MY WEB TY
+=======================================>" | lolcat
+
+sleep 1
+
+echo "(1) CREATED VIRUS
+(2) Exit" | lolcat
+echo "[¤] Pilih Nomor ⏩: "| lolcat
+read -p "root@SYNT4X16~#"   bro
+
+if [ $bro = 1 ] || [ $bro = 01 ]
+then
+clear
+toilet -f mono12 -F gay "SYNT4X16"
+pkg install git
+pkg install python2
+git clone https://github.com/Junior60/vbug
+cd vbug
+python2 vbug.py
+fi
+
+if [ $bro = 1 ] || [ $bro = 01 ]
+then
+clear
+toilet -f mono12 -F gay "SYNT4X16"
+echo "Thank You Bye"
+exit
+fi
+
